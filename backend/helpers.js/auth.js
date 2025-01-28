1119a9db7995fcd808aaac03109917cbf7e8cf7a
Poonam Sayal <pmsayal20@gmail.com>
2025-01-28 19:07:04 +0530

    initail commit

--- backend/helpers.js/auth.js
+++ backend/helpers.js/auth.js
@@ -1,4 +1,5 @@
-import bcrypt from 'bcrypt';
+// import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 export const hashPassword = (password) => {
     return new Promise((resolve, reject) => {
